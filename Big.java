e0d782ed2e19b57eb46bacc80f2ef9789356e6ea
vihan2021 <155952251+vihan2021@users.noreply.github.com>
2024-01-09 16:23:38 +0530

    Add files via upload

---
+++ Big.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Big
+
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter two nos");
+                                        int a,b;
+		a= s.nextInt();
+		b= s.nextInt();
+		if(a>b)
+		{
+			System.out.println(a+" is biggest");
+		}
+		else
+		{
+		     System.out.println(b+" is biggest");
+		}
+	
+	}
+}
